1e91cac7ae0704f0b9a38e83870f6837f010f9d6
galihanggara68 <galihanggara68@gmail.com>
2022-02-17 11:43:57 +0700

    * Changed instance_type to get from db_instance_classes

--- modules/sql_server/main.tf
+++ modules/sql_server/main.tf
@@ -10,9 +10,9 @@ resource "alicloud_db_instance" "instance" {
   engine           = var.engine
   engine_version   = var.engine_version
   instance_name    = var.instance_name
-  instance_type    = var.instance_type
+  instance_type    = var.engine == "PostgreSQL" ? data.alicloud_db_instance_classes.default.instance_classes.0.instance_class : var.instance_type
   instance_storage = var.storage
-  db_instance_storage_type = var.engine == "PostgreSQL" ? data.alicloud_db_instance_classes.default.0.storage_type : null
+  db_instance_storage_type = var.engine == "PostgreSQL" ? var.storage_type : null
   vswitch_id       = var.vswitch_id
 }
 
